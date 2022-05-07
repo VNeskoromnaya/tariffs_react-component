9ea5ce33122f9173686d15e7dd79c69cf88d4e53
Valeriia Neskoromnaya <81985957+VNeskoromnaya@users.noreply.github.com>
2022-05-07 11:05:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import './App.css';
 import Card from './components/Card';
 
 
-// const cards = [
-//   { title: 'Безлимитный 300', price: '300', speed: '10', color: 'lazur'},
-//   { title: 'Безлимитный 450', price: '450', speed: '50', color: 'green'},
-//   { title: 'Безлимитный 550', price: '550', speed: '100', color: 'red', isSelected: true},
-//   { title: 'Безлимитный 1000', price: '1000', speed: '200', color: 'black'}
-//   ];
-
-  const cards = [
+const cards = [
     { title: 'Безлимитный 300', price: '300', speed: '10'},
     { title: 'Безлимитный 450', price: '450', speed: '50'},
     { title: 'Безлимитный 550', price: '550', speed: '100',isSelected: true},
